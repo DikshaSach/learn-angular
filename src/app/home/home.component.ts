6bc2410391397ba49d22ee2132202f217b177d29
Diksha Sach <sachdiksha@gmail.com>
2018-05-31 12:06:03 -0400

    adding home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  itemCount: number;
+  btnText: string = 'Add an item';
+  goalText: string = 'My First Life Goal.';
+  goals = [];
+  constructor() { }
+// lifecycle hook initiates on component loading
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+}
